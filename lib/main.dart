9918d2846a8771c9bff2d114fa77e47aa62d0a3e
Dhirajchandel01 <135970076+Dhirajchandel01@users.noreply.github.com>
2023-12-28 17:00:26 +0530

    hhhh

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'location_page.dart';
 
 void main() {
   runApp(const MyApp());
+
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  //  add This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: LocationPage()
+      home: const LocationPage()
     );
   }
 }
